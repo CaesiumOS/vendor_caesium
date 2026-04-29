bf6ea8bff0c3f16b95747f88f9b843c34647e77e
jpb <jpb44173@gmail.com>
2026-04-29 20:06:49 +0100

    PermissionController M3E fixes
    This fixes a regression present in BP4A, where PermissionController does not have M3E styling
    
    Change-Id: If66789a758fcfe1ff428b0c5a2bd63a78e4cd162

--- config/common.mk
+++ config/common.mk
@@ -282,6 +282,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-crdroid-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-crdroid-product.xml
 
+# Enable Material Design 3 Expressive
+PRODUCT_PRODUCT_PROPERTIES += is_expressive_design_enabled=true
+
+
 # Blur
 ifneq ($(TARGET_SUPPORTS_BLUR),false)
 PRODUCT_PRODUCT_PROPERTIES += ro.surface_flinger.supports_background_blur=1
